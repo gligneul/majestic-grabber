0408d221572193adc67e9febdc4f12c13767a150
Gabriel de Quadros Ligneul <gqligneul@gmail.com>
2020-01-26 21:53:57 -0300

    Use youtubedl library directly instead of using the binary

--- majesticgrabber/mp3dl.py
+++ majesticgrabber/mp3dl.py
@@ -7,9 +7,16 @@ from youtube_dl.YoutubeDL import YoutubeDL
 YOUTUBE_WATCH = "https://www.youtube.com/watch?v="
 
 def download(id, path):
-    print(id, path)
-
-#        logging.info("downloading video " + id + " as " + path)
-#        cmd = [self.ytdl, YOUTUBE_WATCH + id, "-x", "--audio-format", "mp3",
-#                "--audio-quality", "0", "-o", path]
+    postprocessors = [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '0',
+        'nopostoverwrites': False
+    }]
+    ydl_opts = {
+        'postprocessors': postprocessors,
+        'outtmpl': path
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([YOUTUBE_WATCH + id])
 
